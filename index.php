4d4d9fed47504ebc48dbbf340ff0d8cc2084b856
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-30 19:55:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php
+
 <html>
   <body aligh="center">
   <img align="center" src="http://www.osius.com/wp-content/themes/osius/images/logo.png"/> 
-  <h1>echo gethostname();</h1>
-echo "\n";
+  <h1><?php echo gethostname();?></h1>
+<?php echo "\n"; ?>
 </body>
-  </html>
-?>
+</html>
