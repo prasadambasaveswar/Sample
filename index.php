f511ba26f889da32c1a063bcb2827a3f88ea8b02
prasadambasaveswar <basaveswar01@gmail.com>
2018-05-01 17:25:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <body aligh="center" width="100%">
   <table width="100%"><img align="center" src="http://www.osius.com/wp-content/themes/osius/images/logo.png"/> 
-  <h1><?php echo "THIS IS TESTING OF MANUAL BUILDING";?></h1>
+  <h1><?php echo "THIS IS TESTING OF AU BUILDING";?></h1>
 <?php echo "\n"; ?>
     </table>
 </body>
