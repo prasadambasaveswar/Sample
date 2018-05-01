9ceb218fa210e86e42871dcdf605c76732c3186a
prasadambasaveswar <basaveswar01@gmail.com>
2018-05-01 17:03:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php
-echo "POD NAME is - ";
-echo gethostname();
-?>
+<html>
+  <body aligh="center" width="100%">
+  <table width="100%"><img align="center" src="http://www.osius.com/wp-content/themes/osius/images/logo.png"/> 
+  <h1><?php echo "THIS IS TESTING OF MANUAL BUILDING";?></h1>
+<?php echo "\n"; ?>
+    </table>
+</body>
+</html>
