c5c5beab0b76d60bbbb857c336a0799e6c339ecd
prasadambasaveswar <basaveswar01@gmail.com>
2023-10-05 19:35:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <body aligh="center" width="100%">
   <table width="100%"><img align="center" src="http://www.osius.com/wp-content/themes/osius/images/logo.png"/> 
     <color>
-    <marquee><h1><?php echo "OpenShift <font color=\"blue\">Automation</font> With Ansible.";?></h1></marquee>
+    <marquee><h1><?php echo "OpenShift <font color=\"blue\">Automation</font> With Ansible Demo.";?></h1></marquee>
 <?php echo "\n"; ?>
     </table>
 </body>
